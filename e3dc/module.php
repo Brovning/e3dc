5e6b7f818ab48d6c066292114e9216e7b6a64e8b
Brovning <39644775+Brovning@users.noreply.github.com>
2019-10-31 14:26:13 +0100

    initial commit

---
+++ e3dc/module.php
@@ -0,0 +1,27 @@
+<?php
+	class E3DC extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+			//Properties
+			$this->RegisterPropertyString('IP', '');
+//			$this->RegisterPropertyString('Password', '');
+
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+	}
