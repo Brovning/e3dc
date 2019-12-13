af4bd7f92860c2c83761ca29b194df7b28e20848
Brovning <39644775+Brovning@users.noreply.github.com>
2019-12-13 21:28:44 +0100

    added File Validation Test

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -1,14 +1,14 @@
 <?php
 declare(strict_types=1);
 include_once __DIR__ . './Validator.php';
-class SymconCoreStubsValidationTest extends TestCaseSymconValidation
+class FileValidationTest extends TestCaseSymconValidation
 {
     public function testValidateCoreStubs(): void
     {
-        $this->validateLibrary(__DIR__ . './CoreStubs');
+        $this->validateLibrary(__DIR__ . '/../');
     }
     public function testValidateDNSSDControl(): void
     {
-        $this->validateModule(__DIR__ . './CoreStubs/DNSSDControl');
+        $this->validateModule(__DIR__ . '/../e3dc');
     }
 }
