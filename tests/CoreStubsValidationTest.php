44bc7d6d818a4ddae746a9d7674db1427e688c40
Brovning <39644775+Brovning@users.noreply.github.com>
2019-12-13 21:11:32 +0100

    Update CoreStubsValidationTest.php

--- tests/CoreStubsValidationTest.php
+++ tests/CoreStubsValidationTest.php
@@ -1,14 +1,14 @@
 <?php
 declare(strict_types=1);
-include_once __DIR__ . '/../../tests/Validator.php';
+include_once __DIR__ . './Validator.php';
 class SymconCoreStubsValidationTest extends TestCaseSymconValidation
 {
     public function testValidateCoreStubs(): void
     {
-        $this->validateLibrary(__DIR__ . '/../../tests/CoreStubs');
+        $this->validateLibrary(__DIR__ . './CoreStubs');
     }
     public function testValidateDNSSDControl(): void
     {
-        $this->validateModule(__DIR__ . '/../../tests/CoreStubs/DNSSDControl');
+        $this->validateModule(__DIR__ . './CoreStubs/DNSSDControl');
     }
 }
