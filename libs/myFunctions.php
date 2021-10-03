3b9614e876d388777e347582e315372406dd2a3a
Brovning <39644775+Brovning@users.noreply.github.com>
2021-10-03 11:14:47 +0200

    Erweitert um RegisterHook() und GetMimeType()

--- libs/myFunctions.php
+++ libs/myFunctions.php
@@ -490,6 +490,48 @@ trait myFunctions
         return $bufferSum;
     }
 
+    // Inspired by module SymconTest/HookServe
+    private function RegisterHook($WebHook)
+    {
+        $ids = IPS_GetInstanceListByModuleID('{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}');
+        if (count($ids) > 0) {
+            $hooks = json_decode(IPS_GetProperty($ids[0], 'Hooks'), true);
+            $found = false;
+            foreach ($hooks as $index => $hook) {
+                if ($hook['Hook'] == $WebHook) {
+                    if ($hook['TargetID'] == $this->InstanceID) {
+                        return;
+                    }
+                    $hooks[$index]['TargetID'] = $this->InstanceID;
+                    $found = true;
+                }
+            }
+            if (!$found) {
+                $hooks[] = ['Hook' => $WebHook, 'TargetID' => $this->InstanceID];
+            }
+            IPS_SetProperty($ids[0], 'Hooks', json_encode($hooks));
+            IPS_ApplyChanges($ids[0]);
+        }
+    }
+
+    // Inspired by module SymconTest/HookServe
+    private function GetMimeType($extension)
+    {
+        $lines = file(IPS_GetKernelDirEx() . 'mime.types');
+        foreach ($lines as $line) {
+            $type = explode("\t", $line, 2);
+            if (count($type) == 2) {
+                $types = explode(' ', trim($type[1]));
+                foreach ($types as $ext) {
+                    if ($ext == $extension) {
+                        return $type[0];
+                    }
+                }
+            }
+        }
+        return 'text/plain';
+    }
+
     // ermittelt die InstanzId des Archive Controls (Datanbank des Variablen-Loggings)
     private function getArchiveId()
     {
