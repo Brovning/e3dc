d9d433169e5e3675f4b708d64b73fdd2f9c76c9b
Brovning <39644775+Brovning@users.noreply.github.com>
2022-01-20 19:31:35 +0100

    dynamic module detection

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -1,14 +1,25 @@
 <?php
+
 declare(strict_types=1);
-include_once __DIR__ . '/stubs/Validator.php';
+include_once __DIR__.'/stubs/Validator.php';
 class FileValidationTest extends TestCaseSymconValidation
 {
-    public function testValidateCoreStubs(): void
-    {
-        $this->validateLibrary(__DIR__ . '/../');
-    }
-    public function testValidateDNSSDControl(): void
-    {
-        $this->validateModule(__DIR__ . '/../e3dc');
-    }
+	public function testValidateCoreStubs(): void
+	{
+		$this->validateLibrary(__DIR__.'/../');
+	}
+
+	public function testValidateDNSSDControl(): void
+	{
+		$dirs = array_filter(glob(__DIR__.'/../*'), 'is_dir');
+
+		foreach ($dirs as $dir)
+		{
+			if (file_exists($dir."/module.php"))
+			{
+				echo "\nvalidateModule(): ".$dir;
+				$this->validateModule($dir);
+			}
+		}
+	}
 }
