4f910eb74ef766c91615a4f94590c5561b7f7ca5
Brovning <39644775+Brovning@users.noreply.github.com>
2022-01-20 19:45:34 +0100

    class name changed to ValidationTest
    Warning:       Test case class not matching filename is deprecated
                   in /home/runner/work/e3dc/e3dc/tests/ValidationTest.php
                   Class name was 'FileValidationTest', expected 'ValidationTest'

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 include_once __DIR__.'/stubs/Validator.php';
-class FileValidationTest extends TestCaseSymconValidation
+class ValidationTest extends TestCaseSymconValidation
 {
 	public function testValidateCoreStubs(): void
 	{
